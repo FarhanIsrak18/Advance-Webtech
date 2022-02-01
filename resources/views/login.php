695aeaca855672f1e1cb07023ad6840b61a6aeb3
FarhanIsrak18 <shahidinfo.45@gmail.com>
2022-02-01 20:19:25 +0600

    first task

---
+++ resources/views/login.php
@@ -0,0 +1,7 @@
+<html>
+    <form action="" method="post">
+        Enter name<input type="text" value="" name="name">
+        Enter password<input type="password" value="" name="password">
+        <input type="submit" value="submit" name="submit">
+</form>
+    </html>
